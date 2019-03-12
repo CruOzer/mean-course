25e1ea362ac77bcd65632bd9737fedf14e8e1666
CruOzer <ChrisKost94@gmail.com>
2019-03-12 15:55:42 +0100

    CORS-URL in .env

--- backend/app.js
+++ backend/app.js
@@ -22,10 +22,9 @@ mongoose.connect(process.env.MONGO_URI, {
     console.log('MongoDB connection failure: ' + err);
   });
 
-
 // Allow CORS
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin',  process.env.CORS_URL);
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   next();

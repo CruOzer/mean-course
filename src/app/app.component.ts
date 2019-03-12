57d4e5a2f36551eba52a57b12e58265d8cd2ab80
CruOzer <ChrisKost94@gmail.com>
2019-03-12 12:10:59 +0100

    Finalize Authentication

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    this.authService.autoAuthUser();
+  }
+  constructor(private authService: AuthService) {}
   title = 'mean-course';
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,11 +1,11 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { AuthData } from '../models/AuthData';
-import { Subject, Observable } from 'rxjs';
-import { Router } from '@angular/router';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { AuthData } from "../models/AuthData";
+import { Subject, Observable } from "rxjs";
+import { Router } from "@angular/router";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class AuthService {
   private isAuthenticated = false;
@@ -20,7 +20,7 @@ export class AuthService {
       password
     };
     this.http
-      .post('http://localhost:5000/api/user/signup', authData)
+      .post("http://localhost:5000/api/user/signup", authData)
       .subscribe(response => {
         console.log(response);
       });
@@ -33,28 +33,42 @@ export class AuthService {
     };
     this.http
       .post<{ token: string; expiresIn: number }>(
-        'http://localhost:5000/api/user/login',
+        "http://localhost:5000/api/user/login",
         authData
       )
       .subscribe(response => {
         this.token = response.token;
         if (this.token) {
           const expiresInDuration = response.expiresIn;
-          this.tokenTimer = setTimeout(() => {
-            this.logout();
-          }, expiresInDuration * 1000);
-          this.isAuthenticated = true;
-          this.authStatusListener.next(true);
-          this.router.navigate(['/']);
+          this.setAuthTime(expiresInDuration);
+          this.setAuthentication(true);
+          // Save Data
+          const now = new Date();
+          const expirationDate = new Date(
+            now.getTime() + expiresInDuration * 1000
+          );
+          this.saveAuthData(this.token, expirationDate);
+          this.router.navigate(["/"]);
         }
       });
   }
+  private setAuthTime(duration: number) {
+    this.tokenTimer = setTimeout(() => {
+      this.logout();
+    }, duration * 1000);
+  }
+
+  private setAuthentication(auth: boolean) {
+    this.isAuthenticated = auth;
+    this.authStatusListener.next(auth);
+  }
+
   logout() {
     this.token = null;
-    this.isAuthenticated = false;
-    this.authStatusListener.next(false);
+    this.setAuthentication(false);
     clearTimeout(this.tokenTimer);
-    this.router.navigate(['/']);
+    this.clearAuthData();
+    this.router.navigate(["/"]);
   }
 
   getToken(): string {
@@ -67,5 +81,39 @@ export class AuthService {
 
   getAuthStatusListener(): Observable<boolean> {
     return this.authStatusListener.asObservable();
+  }
+
+  autoAuthUser() {
+    const info: { token: string; expirationDate: Date } = this.getAuthData();
+    if (!info) { return; }
+    const now = new Date();
+    const expiresIn = info.expirationDate.getTime() - now.getTime();
+    if (expiresIn > 0) {
+      this.token = info.token;
+      this.setAuthTime(expiresIn / 1000);
+      this.setAuthentication(true);
+    }
+  }
+
+  private getAuthData(): { token: string; expirationDate: Date } {
+    const token = localStorage.getItem("tokenMeanCourse");
+    const expirationDate = localStorage.getItem("expirationMeanCourse");
+    if (!token && !expirationDate) {
+      return;
+    }
+    return {
+      token,
+      expirationDate: new Date(expirationDate)
+    };
+  }
+
+  private saveAuthData(token: string, expirationDate: Date) {
+    localStorage.setItem("tokenMeanCourse", token);
+    localStorage.setItem("expirationMeanCourse", expirationDate.toISOString());
+  }
+
+  private clearAuthData() {
+    localStorage.removeItem("tokenMeanCourse");
+    localStorage.removeItem("expirationMeanCourse");
   }
 }

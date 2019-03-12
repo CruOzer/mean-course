d06784eb1c590e880a205f05c73f7026ff996d51
CruOzer <ChrisKost94@gmail.com>
2019-03-12 11:42:27 +0100

    AuthGuard

--- src/app/components/auth/login/login.component.ts
+++ src/app/components/auth/login/login.component.ts
@@ -1,20 +1,21 @@
-import { Component, OnInit } from "@angular/core";
-import { NgForm } from "@angular/forms";
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
-  selector: "app-login",
-  templateUrl: "./login.component.html",
-  styleUrls: ["./login.component.scss"]
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
   isLoading: boolean = false;
   constructor(private authService: AuthService) {}
 
   onLogin(form: NgForm) {
-    if(!form.valid){
-      return
+    if (!form.valid) {
+      return;
     }
+    this.isLoading = true;
     this.authService.login(form.value.email, form.value.password);
   }
   ngOnInit() {}
